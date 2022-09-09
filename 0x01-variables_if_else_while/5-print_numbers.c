23d2f438086fb43f4d1f3be9d8fe57298d3a7a38
Chinedu <georgechinedu2018@gmail.com>
2022-09-09 16:16:08 -0700

    completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
