ccafb99b4ea9306a66516287f2cf487cdf0ffeac
Chinedu <georgechinedu2018@gmail.com>
2022-09-09 00:15:11 -0700

    Complete

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: program that prints the size of various
+ * types on the screen
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char
+	int
+	long int
+	long long int
+	float
+}
