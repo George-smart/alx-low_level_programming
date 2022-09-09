9c03f74f171d7a8ddc9ff5bc3087c6bf596ac1f3
Chinedu <georgechinedu2018@gmail.com>
2022-09-09 15:45:38 -0700

    completed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - entry point
  *
@@ -12,5 +14,6 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+		putchar('\n');
 	return (0);
 }
