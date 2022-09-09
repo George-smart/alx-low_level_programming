b40e586728cd9dfae0eb780df10f681c1d145554
Chinedu <georgechinedu2018@gmail.com>
2022-09-09 07:17:12 -0700

    Completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints to the standard error
+ *
+ * Description: prints "and that piece of art is useful" -
+ * Dora Korpar, 2015-10-19
+ * Return: one
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"\
+- Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
