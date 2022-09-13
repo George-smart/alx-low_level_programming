f710aedcaf3ac8865fb19c6eea1bd3d453bf1a50
Chinedu <georgechinedu2018@gmail.com>
2022-09-13 12:09:04 -0700

    completed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - check for absolute value
+ *@i: check for absolute of i
+ * Description: check for absolute value
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	int c = abs(i);
+
+	_putchar(c);
+	return (0);
+}
