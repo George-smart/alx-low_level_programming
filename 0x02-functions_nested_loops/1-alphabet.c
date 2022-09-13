577a8d39f181fae38d720c6b0afa8c2acba9cfe4
Chinedu <georgechinedu2018@gmail.com>
2022-09-13 10:10:39 -0700

    Completed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,6 +10,8 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c < 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,3 @@
  *
  * Return: none;
  */
-void print_alphabet_x10(void)
-{
-    char c;
-    int i = 10;
-
-    while (i < 10)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-    }
-
-    return;
-}
