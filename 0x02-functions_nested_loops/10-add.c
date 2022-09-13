01b1a1ade7ca31bc5c96f84b11eff44b81275051
Chinedu <georgechinedu2018@gmail.com>
2022-09-13 14:55:27 -0700

    completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - addition function
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Adding of two integer no
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
