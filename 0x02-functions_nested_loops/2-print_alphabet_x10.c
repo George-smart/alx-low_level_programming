8069bb2418255715e242be296ca0b6445a06bd5b
Chinedu <georgechinedu2018@gmail.com>
2022-09-13 10:27:59 -0700

    Completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print function
+ *
+ * Return: none
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 10;
+
+	while (int < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c + '\n');
+		}
+
+		i++;
+	}
+	_putchar('\n')
+}
