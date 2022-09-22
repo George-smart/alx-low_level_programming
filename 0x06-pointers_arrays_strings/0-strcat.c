1de00464b733a6debb14e5209a3db719db8cd746
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-09-22 20:22:47 +0100

    Task 0 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,19 @@
  * Return: char item
  */
 
-char *_strcat(char *dest, char *src)
-{
-	return strcat(dest, src);
-}
+ char *_strcat(char *dest, char *src)
+ {
+    int n = 0, i;
+
+    while (dest[n])
+        n++;
+
+    for (i = 0; src[i] != 0; i++)
+    {
+        dest[n] = src[i];
+        n++;
+    }
+
+    dest[n] = '\0';
+    return (dest);
+ }
