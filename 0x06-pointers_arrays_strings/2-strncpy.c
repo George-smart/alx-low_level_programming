da06ba2fde7cc8807cda3b0869e379ad94e1c55b
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-09-22 20:49:17 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _strncpy - copy function.
+ *@dest: destination parameter.
+ *@src: source text.
+ *@n: number of char to copy.
+ * Return: dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for (; i < n; i++)
+        dest[i] = '\0';
+
+    return dest;
+}
