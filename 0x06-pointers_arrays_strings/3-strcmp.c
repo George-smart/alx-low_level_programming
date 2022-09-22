b491b99900450fa5c8b5123f97e30fd18a2a388f
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-09-22 20:53:10 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * _strcat - concatenation function.
+ *@s1: first param to compare with.
+ *@s2: second param. to compare with.
+ * Return: int.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    int n, i;
+
+    for (i = 0; i < s1[i], s1[i] != '\0'; ++i)
+    {
+        for (n = 0; n < s2[n], s2[n] != '\0'; ++n)
+        {
+            if (s1[i] > s2[n])
+                return (1);
+            else if (s1[i] < s2[n])
+                return (-1);
+            else
+                return (0);
+
+        }
+    }
+
+    return (0);
+}
