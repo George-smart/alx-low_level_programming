997ef5eddc0cf2d40606a769ec3aab9af1307c44
Chinedu <georgechinedu2018@gmail.com>
2022-09-22 12:20:27 -0700

    Task done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcat - concatenation string
+ *@*dest: copying destination.
+ *@*src: copying item.
+ * Return: char item
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
