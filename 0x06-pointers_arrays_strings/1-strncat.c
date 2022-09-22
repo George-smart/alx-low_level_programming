1b662bc04cb557d77a918b2e139137da9b9c8744
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-09-22 20:38:50 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * _strncat - concatenation function.
+ *@dest: destination parameter.
+ *@src: source text.
+ *@n: number of char to concatenate.
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+    int dlen = 0, i;
+
+    while (dest[dlen])
+        dlen++;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[dlen] = src[i];
+        dlen++;
+    }
+
+    dest[dlen] = '\0';
+    return dest;
+}
