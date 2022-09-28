efe3ab6da3034b1a67bffc227f08d639090a88ec
Chinedu <georgechinedu2018@gmail.com>
2022-09-28 12:12:39 -0700

    completed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
-		return;
-
-	_putchar(s[0]) + _puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 #ifndef INV_TREE_H
 #define INV_TREE_H
-
+int _putchar(char c);
 void _puts_recursion(char *s);
 
 #endif
