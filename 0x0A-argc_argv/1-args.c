c60df29f5df9ba394508478949af48f3fe467f91
Chinedu <georgechinedu2018@gmail.com>
2022-09-30 19:38:09 -0700

    completed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argument
+ * @argc: argument count.
+ * @argv: argument list.
+ * Return: 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+
+	return (0);
+}
