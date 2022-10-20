f00f1fe5f3ac6739fb2ceaec93dc7c524918093f
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-21 00:24:15 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,18 +8,17 @@
 * @str: str data of the added node list
 * Return: address of the new element
 */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	char *copystr;
 
 	list_t *newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
 	newNode->str = str;
 	
 	copystr = strdup(str);
 	newNode->next = *head;
 	*head = newNode;
-	if (newNode == NULL)
-		return (NULL);
 	return (newNode);
 }
