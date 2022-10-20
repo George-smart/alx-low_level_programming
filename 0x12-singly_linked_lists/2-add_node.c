7a6c52002cfd057e712ced60126253e07d184dc0
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-21 00:36:19 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
-* add_node - adding to the front of a list
-* @head: head of the list
-* @str: str data of the added node list
-* Return: address of the new element
-*/
+ * add_node - A function that adds a node to the head of linked list
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @str: pointer to string that adds as node
+ * Return: pointer to new head of list, NULL on failure
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	int length = 0;
