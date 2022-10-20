3ed6e1855a6f269ccc49d850c5fe8883b10fe699
George-smart <georgechinedu2018@gmail.com>
2022-10-20 23:23:53 +0100

    complete Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* list_len - function that return the lenght of element
+* @h: List of nodes to check length
+* Return: Number of element in a linked list
+*/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int nodeslen = 0;
+	while (h)
+	{
+		nodeslen++;
+	}
+	
+	return (nodeslen);
+}
