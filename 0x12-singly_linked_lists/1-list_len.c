1a2c6a71ef08853259be60ef3651ab16ccb3f5f0
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-20 23:36:29 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
-* list_len - function that return the lenght of element
-* @h: head of the link
-* Return: number of nodes as size_t
-*/
+ * list_len - A function that gets the number of nodes of linked list
+ * @h: head of linked list
+ * Return: number of nodes as size_t
+ */
 size_t list_len(const list_t *h)
 {
-	unsigned int nodeslen = 0;
-	
+	unsigned int nodes = 0;
+
 	while (h)
 	{
-		nodeslen++;
+		nodes++;
 		h = h->next;
 	}
-	return (nodeslen);
+	return (nodes);
 }
