98c063b25f51ea23d107879e1875c85514f996bb
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-25 20:01:45 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num;
+	size_t num = 0;
 	
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num += 1;
+		num++;
 	}
 	return (num);
 }
