dccf3f4718dce8e7ecf0fea5e695883cad6c4c3b
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-27 19:55:46 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,52 +1,14 @@
-#include "main.h"
-
-#define CHAR_BITS 8 /*size of char*/
-#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
-
 /**
- * print_bin - prints binary rep of @n
+ * get_bit - finds value of bit at a given index
+ * @n: decimal number
+ * @index: index starting from 0
  *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
-{
-	if (number >> 1)
-		print_bin(number >> 1);
-
-	putc((number & 1) ? '1' : '0', stdout);
-}
-
-
-/**
- * get_bit - gets a bit at a given index
- *
- * @n: decimal value
- * @index: integer value of bit position
- *        to get from @n
- *
- * Return: the value of the bit or -1 if an
- *          error occured
-*/
+ * Return: value of bit at index || -1
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
-
-	/*if index is greater than size of n in binary coded decimal*/
-	if (index > INT_BITS)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	#ifdef DEBUG
-	printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
-	printf("%ld in binary: ", n);
-	print_bin(n);
-	printf("\n[%ld >> %d] in binary: ", n, index);
-	print_bin(n >> index);
-	printf(" <- last bit is bit at index %d\n\n", index);
-	#endif
-
-	bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
-
-	return (bit);
+	return (n >> index & 1);
 }
