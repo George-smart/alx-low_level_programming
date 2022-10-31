a2ae4a8dc077991b6bdb2f6a5c461f2613a054d6
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-31 22:39:35 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,6 @@
 #ifndef _MAIN_H
+#define _MAIN_H
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif /* _MAIN_H */
